43a50e588ce467dc316c97741f8978cfd7a1e61b
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-19 16:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+const fontEle = document.querySelector("#fontsize");
+const colorEle = document.querySelector("#fontcolor");
+form.addEventListener("submit", () => {
+  document.cookie = `fontsize=${fontEle.value}`;
+  document.cookie = `fontcolor=${colorEle.value}`;
+});
+
+const cookies = document.cookie.split("; ");
+cookies.shift();
+
+console.log(cookies);
+if (cookies) {
+  const val = [];
+  for (const c of cookies) {
+    const [_, value] = c.split("=");
+    val.push(value);
+  }
+  document.documentElement.style.setProperty("--fontsize", `${val[0]}px`);
+  document.documentElement.style.setProperty("--fontcolor", `${val[1]}`);
+  fontEle.value = val[0];
+  colorEle.value = val[1];
+}
